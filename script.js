0a9b881c15cf26df83e8359f238fdbfa496896f9
Erica Osborn <ericaosborn@Ericas-MacBook-Air.local>
2022-06-28 18:03:37 -0400

    script.js file added

---
+++ script.js
@@ -0,0 +1,20 @@
+function submitReservation() {
+    let fullName = document.getElementById("fullName");
+    let phoneNumber =document.getElementById("phoneNumber")
+    let numberOfAttendees = document.getElementsById("numberOfAttendees");
+    let meal = document.getElementById("meal");
+
+    for(i = 0; i < ele.length; i++) {
+        if(ele[i].checked) {
+            console.log(ele[i].value)
+        }    
+    }
+
+    for(i = 0; i < ele.length; i++) {
+        if(ele[i].checked) {
+            console.log(ele[i].value)
+        }    
+    }
+
+    fetch(`http://localhost:2000`,)
+}
